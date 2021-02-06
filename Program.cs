e555ce0b0ae5718d231811dcbff48d0bf2a1e823
Gabriel <gaplogan@gmail.com>
2021-02-06 10:21:26 -0300

    2021.02.06.10.21

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace cs_1._16_ValidaCorreto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variáveis
+            double num, raiz;
+
+            //{entrada de dados}
+            num = -1; //valor inicial conveniente para testar a validação
+            while (num < 0)
+            {
+                Console.Write("Digite um número positivo: ");
+                num = double.Parse(Console.ReadLine());
+                if (num < 0) // Redundância de verificação, isto pode ser melhorado !!!
+                {
+                    Console.WriteLine("Valor Inválido");
+                }                
+                Console.WriteLine(); // Pular linha"
+            }
+
+            //{Processamento: Tenta extrair a raiz quadrada do número lido}
+            raiz = Math.Sqrt(num);
+
+            Console.WriteLine($"Raiz quadrada de {num}: {raiz}");
+        }
+    }
+}
